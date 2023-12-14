4ac8514b32ee422f6ec7ef4a42d21c2dcb0943b0
Keri Sen <keri.l.sen@gmail.com>
2023-12-13 20:15:54 -0600

    Added some Foundation styling and tried to get the Flickr API to display images

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # Neighbor-Share
 
-
 # Description
 Neighbor Share is a landing page that helps people, in their community, connect with each other to share or borrow specific items.
-
 
 # User Story
 
@@ -11,8 +9,21 @@ As a neighbor
 I Want to communicate with people within the neighborhood
 So That I can borrow or share items within my community.
 
-
 # Acceptance Criteria
+
+AS A Neighbor who wants to borrow and share items within my community,
+WHEN I enter my address,
+THEN I get a map within five miles of my address.
+WHEN I get a map,
+THEN I get a unique ID.
+WHEN I get a unique ID that defines my space,
+THEN I can share it with my neighbors and use it for future access.
+WHEN I share it with my neighbors, 
+THEN I am able to upload photos/listings for items to borrow.
+WHEN I load the page with my unique ID,
+THEN I am provided with listings that match items in my neighborhood.
+WHEN I click on item listings,
+THEN I am provided with a modal window with further information and an option to contact the neighbor who owns the item.
 
 Click on the Get Started tab and search for your neighborhood by your city and zip code. Once your neighborhood shows on the screen, you should be given the option to either borrow or lend an item. Once you select the lender option, feel free to post your shared item or wait for someone to post the item they need to borrow. Once you select the borrow option, please post from the filter section of the item you need to borrow.
 
@@ -20,7 +31,9 @@ Click on the Get Started tab and search for your neighborhood by your city and z
 # Visuals
 (Screenshot or link to homepage)
 
-
+# Credits
+https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
+fetch Flickr API algorithm from ChatGPT
 
 # Authors and acknowledgment
 Benita Isaac, Keri Sen, Justin Hill, DonAndre Clark II, Monet Montgomery
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 
 function initMap() {
-    // console.log("Maps JavaScript API loaded.");
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 37.7749, lng: -122.4194 },
-        zoom: 12
-    });
+   // console.log("Maps JavaScript API loaded.");
+   var map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 37.7749, lng: -122.4194 },
+      zoom: 12
+   });
 
-     // Create a Places Autocomplete object for the search input field
+   // Create a Places Autocomplete object for the search input field
    var input = document.getElementById('addressInput');
    var autocomplete = new google.maps.places.Autocomplete(input);
 
@@ -14,7 +14,7 @@ function initMap() {
    autocomplete.bindTo('bounds', map);
 
    // Add an event listener for when a place is selected
-   autocomplete.addListener('place_changed', function() {
+   autocomplete.addListener('place_changed', function () {
       var place = autocomplete.getPlace();
 
       if (!place.geometry) {
@@ -31,5 +31,57 @@ function initMap() {
       }
    });
 }
-  
+document.addEventListener("DOMContentLoaded", function () {
+   const apiKey = 'f855e062782300ad36a1dc15d727ecff';
+   const userId = "199652929@N05"; // Your Flickr user ID
+   const photoContainer = document.getElementById('photo-container');
+
+   // Flickr API endpoint for fetching recent photos
+   const flickrEndpoint = `https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=${apiKey}&user_id=${userId}&format=json&nojsoncallback=1`;
+
+   // Fetch data from Flickr API
+   fetch(flickrEndpoint)
+      .then(response => response.json())
+      .then(data => {
+         // Extract photo information from the API response
+         const photos = data.photos.photo;
+
+         // Create HTML elements for each photo and append them to the container
+         photos.forEach(photo => {
+            const photoUrl = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
+            const imgElement = document.createElement('img');
+            imgElement.src = photoUrl;
+            imgElement.alt = photo.title;
+
+            // You can customize this part to add more information or styling
+            const photoDiv = document.createElement('div');
+            /*photoDiv.addClass("featured-image-block column");*/
+            photoDiv.appendChild(imgElement);
+            /*console.log(photoDiv);
+            const pClass = document.createElement('p');
+            pClass.addClass("text-center featured-image-block-title");
+            pClass.appendChild(photo.title);
+            photoDiv.appendChild(pClass);
+            console.log(photoDiv);*/
+
+            photoContainer.appendChild(photoDiv);
+         });
+      })
+      .catch(error => console.error('Error fetching data from Flickr API:', error));
+});
+
 //   window.initMap = initMap;
+function copyToClipboard() {
+   // Get the text field
+   var copyText = document.getElementById("myInput");
+
+   // Select the text field
+   copyText.select();
+   copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+   navigator.clipboard.writeText(copyText.value);
+
+   // Alert the copied text
+   alert("Copied the text: " + copyText.value);
+}
