5227443ac742c9d3b8cdec3b618e6da5a1bf5e7e
Keri Sen <keri.l.sen@gmail.com>
2023-12-18 16:17:46 -0600

    So close to getting the image filter to work

--- assets/js/photos.js
+++ assets/js/photos.js
@@ -1,10 +1,31 @@
-//Event listener for Flickr 
+const apiKey = 'f855e062782300ad36a1dc15d727ecff';
+const photoContainer = document.getElementById('photo-container');
+
+// Run each photo id through Flickr method flickr.tags.getListPhoto to get the tag list
+function getTags(x) {
+
+    const photo_id = x;
+    const imageContainer = document.getElementById(photo_id);
+    const flickrTagslist = `https://api.flickr.com/services/rest/?method=flickr.tags.getListPhoto&api_key=${apiKey}&photo_id=${photo_id}&format=json&nojsoncallback=1`;
+    let tagList = "";
+    fetch(flickrTagslist)
+        .then(response => response.json())
+        .then(data => {
+            const tags = data.photo.tags.tag;
+            // tagList = "";
+            tags.forEach(tag => {
+                const userTag = tag._content;
+                imageContainer.classList.add(userTag);
+            })
+        })
+}
+
+//Event listener for Flickr API pull
 document.addEventListener("DOMContentLoaded", function () {
-    const apiKey = 'f855e062782300ad36a1dc15d727ecff';
-    const photoContainer = document.getElementById('photo-container');
+
     const tags = 'neighbor-share, Dublin, 94568, happy-82ay9J5';
 
-    // Flickr API endpoint for fetching recent photos
+    // Flickr API endpoint for fetching photos with user-specific tags
     const flickrEndpoint = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${tags}&tag_mode=all&format=json&nojsoncallback=1`;
 
     // Fetch data from Flickr API
@@ -14,6 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
             // Extract photo information from the API response
             const photos = data.photos.photo;
 
+            // Create separate function to do that
             // Create HTML elements for each photo and append them to the container
             photos.forEach(photo => {
                 const photoUrl = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
@@ -22,9 +44,22 @@ document.addEventListener("DOMContentLoaded", function () {
                 imgElement.alt = photo.title;
 
                 // You can customize this part to add more information or styling
+
                 const photoDiv = document.createElement('div');
                 photoDiv.appendChild(imgElement);
+                photoDiv.setAttribute('id', photo.id);
+                console.log(photo.id);
                 photoDiv.classList.add('column', 'column-block', 'filter-simple-item');
+                getTags(photo.id);
+                /*
+                                if (tagsArr.includes('outdoor')) { photoDiv.classList.add('outdoor') };
+                                if (tagsArr.includes('tools')) { photoDiv.classList.add('tools') };
+                                if (tagsArr.includes('camping')) {
+                                    photoDiv.classList.add('camping');
+                                }
+                                else if (tagsArr.includes('events')) {
+                                    photoDiv.classList.add('events');
+                                }*/
                 console.info(imgElement);
                 photoContainer.appendChild(photoDiv);
             });
