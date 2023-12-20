d57885a5880dcf9ab752a37abace8ae262628d13
Keri Sen <keri.l.sen@gmail.com>
2023-12-20 15:21:26 -0600

    Customized neighbor messages and fixed some bugs and formatting

--- assets/js/photos.js
+++ assets/js/photos.js
@@ -2,8 +2,8 @@ const apiKey = 'f855e062782300ad36a1dc15d727ecff';
 const photoContainer = document.getElementById('photo-container');
 
 // Run each photo id through Flickr method flickr.tags.getListPhoto to get the tag list
+// Apply tags as classes in the containing div for the photo filter
 function getTags(x) {
-    console.log("1");
     const photo_id = x;
     var photoDiv = document.getElementById(x);
     const flickrTagslist = `https://api.flickr.com/services/rest/?method=flickr.tags.getListPhoto&api_key=${apiKey}&photo_id=${photo_id}&format=json&nojsoncallback=1`;
@@ -11,7 +11,6 @@ function getTags(x) {
         .then(response => response.json())
         .then(data => {
             const tags = data.photo.tags.tag;
-            // tagList = "";
             tags.forEach(tag => {
                 const userTag = tag._content;
                 console.log(userTag);
@@ -20,42 +19,38 @@ function getTags(x) {
         })
 }
 
+// The "Go" button in step one of the user flow i.e. "Enter your key:"
 let searchButton = document.getElementById('searchKey');
 
 searchButton.addEventListener("click", function () {
     let input = document.getElementById("yourKey");
-
+    let filterEl = document.getElementById("listingsFilter");
+    filterEl.classList.remove("hidden");
     console.log("input is " + input.value);
-
     const tags = 'neighbor-share, ' + input.value;
     console.log("tags are " + tags);
     // Flickr API endpoint for fetching photos with user-specific tags
     const flickrEndpoint = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${tags}&tag_mode=all&format=json&nojsoncallback=1`;
 
-    // Fetch data from Flickr API
+    // Fetch data from Flickr API - starter code from ChatGPT
     fetch(flickrEndpoint)
         .then(response => response.json())
         .then(data => {
-            // Extract photo information from the API response
             const photos = data.photos.photo;
-
-            // Create separate function to do that
-            // Create HTML elements for each photo and append them to the container
+            // Extracting information about each photo from API pull
             photos.forEach(photo => {
                 const photoUrl = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
                 const imgElement = document.createElement('img');
                 imgElement.src = photoUrl;
                 imgElement.alt = photo.title;
 
-                // You can customize this part to add more information or styling
-
                 const photoDiv = document.createElement('div');
                 photoDiv.appendChild(imgElement);
-                // photoDiv.setAttribute('id', photo.id);
-                // console.info(photoDiv);
                 photoDiv.classList.add('column', 'column-block', 'filter-simple-item');
+                // Applying photo.id as an div id to match the photo container to the photo tags
                 photoDiv.id = photo.id;
                 photoContainer.appendChild(photoDiv);
+                // Calling on function to add tags to photo container
                 getTags(photo.id);
             });
         })
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,8 @@ $('#copy').on('click', () => {
    navigator.clipboard.writeText($('#input').val());
 });
 
+// Generates message to share key with neighbors
+function generateMessage() {
+   let message = "Hi Neighbor! I just set up a new neighbor lending space at Neighbor Share.<br><br>You will be able to borrow and lend all sorts of items exclusively with our neighbors. You can access it using the passcode below:<br><br>";
+
+}
